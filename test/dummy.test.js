6830881e2f44859bf06ffd647e00fd394c9a51f8
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-05-23 13:23:46 +0200

    dummy test pass

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 
 describe('Dummy Test', () => {
   it('should pass', () => {
-    assert.strictEqual(true, false);
+    assert.strictEqual(true, true);
   });
 });
