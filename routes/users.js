318fe18772b3950fe4fcd03b3d2a680b5fc9bbdf
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-05-23 12:57:42 +0200

    eslint, prettier, fix

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ members: ["ZZ", "AG", "FG", "JB"] });
+router.get('/', (req, res) => {
+  res.json({ members: ['ZZ', 'AG', 'FG', 'JB'] });
 });
 
 module.exports = router;
