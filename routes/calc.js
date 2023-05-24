df87d9c1aa6040b917025e2fddfc0b314ef42d8e
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-05-24 15:50:18 +0200

    add subtract method and test

--- routes/calc.js
+++ routes/calc.js
@@ -12,4 +12,13 @@ router.get('/add', (req, res) => {
   return res.type('text/plain; charset=utf-8').send(result.toString());
 });
 
+router.get('/subtract', (req, res) => {
+  const { val } = req.query;
+  if (!Array.isArray(val)) {
+    return res.status(400).send('Invalid input');
+  }
+  const result = Calculator.subtract(...val.map(Number));
+  return res.type('text/plain; charset=utf-8').send(result.toString());
+});
+
 module.exports = router;
